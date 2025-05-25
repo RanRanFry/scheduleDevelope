f8a632864a0b60f312585263deeeb5c1e072a183
RanRanFry <burish111@gmail.com>
2025-05-25 14:40:48 +0900

    add : 조회, 수정, 삭제 기능 추가

---
+++ src/main/java/com/example/scheduledevelope/ScheduleDevelopeApplication.java
@@ -0,0 +1,15 @@
+package com.example.scheduledevelope;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScheduleDevelopeApplication {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScheduleDevelopeApplication.class, args);
+    }
+
+
+}
---
+++ src/test/java/com/example/scheduledevelope/ScheduleDevelopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduledevelope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScheduleDevelopeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
